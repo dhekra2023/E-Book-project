597e57b68980068a735fa4e28837623cdec80ee5
dhekra2023 <dhekra202@gmail.com>
2024-09-05 13:44:50 +0100

    v6

--- my-book/src/App.js
+++ my-book/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/navbar";
-//import Footer from "./components/footer/footer";
+// import Footer from "./components/footer/footer";
 import Home from "./components/homePage/home";
 import About from "./components/About/about"; 
-import Contact from "./components/contact/contact";
+
 import Categories from './components/categories/categories'
 import Register from './components/auth/register'
 import Login from './components/auth/login'
@@ -16,7 +16,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
           <Route path="/categories" element={<Categories />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
